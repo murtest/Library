80dae30c8fc30bab4d3f704ac43d398177a22734
murtest <rocketdead1@gmail.com>
2023-04-02 12:30:53 +0300

    finished chapter14

--- app/src/main/java/ru/murtest/library/BookDetailFragment.kt
+++ app/src/main/java/ru/murtest/library/BookDetailFragment.kt
@@ -7,16 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import ru.murtest.library.databinding.FragmentBookDetailBinding
 import java.util.*
-
+private const val BUNDLE_KEY_DATE_START = "BUNDLE_KEY_DATE_START"
+private const val BUNDLE_KEY_DATE_END = "BUNDLE_KEY_DATE_END"
 class BookDetailFragment : Fragment() {
 
     private var _binding: FragmentBookDetailBinding? = null
@@ -51,14 +54,6 @@ class BookDetailFragment : Fragment() {
                 }
             }
 
-            bookDateReadStart.apply {
-                isEnabled = false
-            }
-
-            bookDateReadEnd.apply {
-                isEnabled = false
-            }
-
             bookFinished.setOnCheckedChangeListener { _, isChecked ->
                 if (!isChecked) {
                     bookDetailViewModel.updateBook { oldBook ->
@@ -84,6 +79,20 @@ class BookDetailFragment : Fragment() {
                 }
             }
         }
+
+        setFragmentResultListener(
+            DatePickerFragment.REQUEST_KEY_DATE
+        ) { _, bundle ->
+            if (bundle.containsKey(BUNDLE_KEY_DATE_START)){
+                val newDateReadStart = bundle.getSerializable(BUNDLE_KEY_DATE_START) as Date
+                bookDetailViewModel.updateBook { it.copy(dateReadStart = newDateReadStart) }
+            }
+            if (bundle.containsKey(BUNDLE_KEY_DATE_END)){
+                val newDateReadEnd = bundle.getSerializable(BUNDLE_KEY_DATE_END) as Date
+                bookDetailViewModel.updateBook { it.copy(dateReadEnd = newDateReadEnd) }
+            }
+
+        }
     }
 
     private fun updateUi(book: Book) {
@@ -91,8 +100,21 @@ class BookDetailFragment : Fragment() {
             if (bookTitle.text.toString() != book.title) {
                 bookTitle.setText(book.title)
             }
+
             bookDateReadStart.text = book.dateReadStart.toString()
+            bookDateReadStart.setOnClickListener {
+                findNavController().navigate(
+                    BookDetailFragmentDirections.selectDate(book.dateReadStart, BUNDLE_KEY_DATE_START)
+                )
+            }
+
             bookDateReadEnd.text = if (book.isFinished) book.dateReadEnd.toString() else ""
+            bookDateReadEnd.setOnClickListener {
+                findNavController().navigate(
+                    BookDetailFragmentDirections.selectDate(book.dateReadEnd, BUNDLE_KEY_DATE_END)
+                )
+            }
+
             bookFinished.isChecked = book.isFinished
         }
     }
